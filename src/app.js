a13f996ea59fb98644f277337d5c0d64b1710186
Benjamin OLIVIER <benjamin.olivier@cpe.fr>
2014-01-21 15:20:57 +0100

    Add comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /*
-
+    Application principale du projet Robot
 */
 
 
@@ -16,7 +16,7 @@ var port = 8080;
 
 app.listen(port);
 
-//these are the only file types we will support for now
+//support de plusieurs type de fichier
 extensions = {
     ".html" : "text/html",
     ".css" : "text/css",
@@ -26,38 +26,33 @@ extensions = {
     ".jpg" : "image/jpeg"
 };
  
-//helper function handles file verification
+//fonction pour la vérification des fichiers
 function getFile(filePath,res,page404,mimeType){
-    //does the requested file exist?
+    //test de l'éxistence du fichier
     fs.exists(filePath,function(exists){
-        //if it does...
         if(exists){
-            //read the fiule, run the anonymous function
             fs.readFile(filePath,function(err,contents){
                 if(!err){
-                    //if there was no error
-                    //send the contents with the default 200/ok header
+                    //si il n'y a pas d'erreurs, envoie 200/ok header
                     res.writeHead(200,{
                         "Content-type" : mimeType,
                         "Content-Length" : contents.length
                     });
                     res.end(contents);
                 } else {
-                    //for our own troubleshooting
+                    //pour debug
                     console.dir(err);
                 };
             });
         } else {
-            //if the requested file was not found
-            //serve-up our custom 404 page
+            //si le fichier n'existe pas, envoie de la page 404
             fs.readFile(page404,function(err,contents){
-                //if there was no error
                 if(!err){
-                    //send the contents with a 404/not found header 
+                    //envoi de la page avec le 404/not found header 
                     res.writeHead(404, {'Content-Type': 'text/html'});
                     res.end(contents);
                 } else {
-                    //for our own troubleshooting
+                    //debug
                     console.dir(err);
                 };
             });
@@ -65,7 +60,7 @@ function getFile(filePath,res,page404,mimeType){
     });
 };
  
-//a helper function to handle HTTP requests
+// fonction de gestion des requêtes HTTP
 function handler(req, res) {
     var
     fileName = path.basename(req.url) || 'index.html',
@@ -73,17 +68,13 @@ function handler(req, res) {
     localFolder = __dirname + '/',
     page404 = localFolder + '404.html';
  
-    //do we support the requested file type?
+    //test du support de l'extention du fichier
     if(!extensions[ext]){
-        //for now just send a 404 and a short message
+        //si n'est pas supporté, envoie de la page 404
         res.writeHead(404, {'Content-Type': 'text/html'});
         res.end("<html><head></head><body>The requested file type is not supported</body></html>");
     };
  
-    //call our helper function
-    //pass in the path to the file we want,
-    //the response object, and the 404 page path
-    //in case the requestd file is not found
     getFile((localFolder + fileName),res,page404,extensions[ext]);
 };
 
@@ -91,6 +82,7 @@ function handler(req, res) {
 
 //creation du websocket (socket.io)
 io.sockets.on('connection', function (socket) {
+  // pour debug lors de la création du WebSocket
   socket.emit('news', { hello: 'world' });
   socket.on('message', function (data) {
   	console.log(data);
